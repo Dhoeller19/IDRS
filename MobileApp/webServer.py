78dda224ed79209548612b8f2ea1d76ff909428b
David Hoeller <holler.david78@gmail.com>
2017-11-11 15:32:08 +0100

    Added a more complex webserver example.

--- MobileApp/webServer.py
+++ MobileApp/webServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/testButton', methods=['POST'])
+def testButton():
+    print("You just said " + request.form['textInput'])
+    return render_template('index.html', inp = request.form['textInput'])
